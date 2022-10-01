ed34cabe697568d85f7142d06ecf4105c9a60d1f
erictau <erictau04@gmail.com>
2022-09-30 17:08:40 -0700

    Adds searching algos

--- Section 10 Searching Algorithms/search.js
+++ Section 10 Searching Algorithms/search.js
@@ -5,4 +5,31 @@ function linearSearch(arr, target) {
     return -1
 }
 
+function binarySearch(arr, target) {
+    let l = 0, r = arr.length - 1, mid
+    while (l <= r) {
+        mid = Math.floor((l + r)/2)
+        if (arr[mid] === target) return mid
+        if (arr[mid] < target) {
+            l = mid + 1
+        } else if (arr[mid] > target) {
+            r = mid - 1
+        }
+    }
+    return -1
+}
+
+function naiveStringSearch(long, short) {
+    let count = 0
+    for (let i = 0; i < long.length; i++) {
+        for (let j = 0; j < short.length; j++) {
+            if (short[j] !== long[i+j]) break
+            if (j === short.length - 1) count++
+        }
+    }
+    return count
+}
+
 console.log(linearSearch([1,2,3,4,5], 3))
+console.log(binarySearch([1,2,3,4,5], 1))
+console.log(naiveStringSearch("lorie loled", "lol"))
