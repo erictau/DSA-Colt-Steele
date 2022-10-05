15b495f7d7720feed3b2374d5069eb3f0748dc5e
erictau <erictau04@gmail.com>
2022-10-05 14:27:35 -0700

    Optimizes bubblesort

--- Section 11 Bubble Sort/bubblesort.js
+++ Section 11 Bubble Sort/bubblesort.js
@@ -1,8 +1,14 @@
 function bubbleSort(arr) {
+    let swapped
     for (let i = arr.length - 1; i > 0; i--) {
-        for (let j = 0; j < i - 1; j++) {
-            if (arr[j] > arr[j+1]) swap(arr, j, j+1)
+        swapped = false
+        for (let j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr, j, j+1)
+                swapped = true
+            }
         }
+        if (swapped === false) break
     }
     return arr
 }
@@ -12,4 +18,4 @@ function swap(arr, x, y) {
     return
 }
 
-console.log(bubbleSort([3,2,5,65,74,234]))
+console.log(bubbleSort([323, 1, 24, 231, 20]))
